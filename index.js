4ac342521b70f27826f32e8d3301a7fafd586d6e
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-06-13 12:12:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply;
+multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+let num3 = 2;
+let num4 = 8;
+
+let mod = num4 / num3 ;
+
+let max = Math.max(1, 2, 3, 10, 20);
